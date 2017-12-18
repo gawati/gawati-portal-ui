5a8af2b5f11092652eba22b95a3b3fdcd16d605e
kohsah <ashok@hariharan.org.in>
2017-12-18 19:13:16 +0530

    Adding missing polyfills.js

---
+++ src/polyfills.js
@@ -0,0 +1,7 @@
+/**
+ * Use this module to import missing APIs for IE 11 via polyfills
+ */
+
+import 'core-js/fn/array/find';
+import 'core-js/fn/array/includes';
+import 'core-js/fn/number/is-nan';
