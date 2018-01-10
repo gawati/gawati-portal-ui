90c663bec40db97373083c0f153f967019a90574
Arunadevi <l.arunadevi@gmail.com>
2018-01-10 20:31:13 +0530

    404 page

--- src/components/Page.js
+++ src/components/Page.js
@@ -6,12 +6,13 @@ import {getRoute} from '../utils/routeshelper';
 
 //import PageUpperBorder from './PageUpperBorder';
 import TopBar from './TopBar';
+import NoMatch from './NoMatch';
 import HomeContentArea from './HomeContentArea';
 import DocumentContentArea from './DocumentContentArea';
 import ListContentArea from './ListContentArea';
 import PageContentArea from './PageContentArea';
 import Footer from './Footer';
-
+import { Redirect } from 'react-router'
 import {PropsRoute} from '../utils/routeshelper';
 
 class Page extends React.Component {
@@ -19,9 +20,11 @@ class Page extends React.Component {
    render() {
         return (
             <Aux>
-
-                <PropsRoute path="/:routeName/_lang/:lang/*" component={TopBar} i18n={this.props.i18n} />
-                <PropsRoute exact path="/" component={TopBar} i18n={this.props.i18n} />
+                <Switch>
+                    <PropsRoute exact path="/:routeName/_lang/:lang/*" component={TopBar} i18n={this.props.i18n} />
+                    <Redirect exact from="/" to="/index.html"component={TopBar} i18n={this.props.i18n} />
+                    <PropsRoute component={TopBar} i18n={this.props.i18n} />
+                </Switch>
                 <Switch>
                     <PropsRoute exact path="/" component={HomeContentArea}  i18n={this.props.i18n} />
                     <PropsRoute exact path="/index.html" component={HomeContentArea} i18n={this.props.i18n} />
@@ -34,6 +37,7 @@ class Page extends React.Component {
                     <PropsRoute path={ getRoute('search-year') } component={ListContentArea} i18n={this.props.i18n} />
                     <PropsRoute path={ getRoute('search-doclang') } component={ListContentArea} i18n={this.props.i18n} />
                     <PropsRoute path={ getRoute('search-keyword') } component={ListContentArea} i18n={this.props.i18n} />
+                    <PropsRoute component={NoMatch} />
                 </Switch>
                 <PropsRoute path="*" component={Footer}  i18n={this.props.i18n}  />
             </Aux>
