92395ab905c2ab32c75c5467aea0478bb02e9845
Ashok Hariharan <ashok@hariharan.org.in>
2018-02-02 08:22:17 +0000

    Revert "Fixes Issue 16"
    This reverts merge request !28

--- src/containers/DocumentContentColumn.js
+++ src/containers/DocumentContentColumn.js
@@ -124,7 +124,6 @@ class DocumentContentColumn extends React.Component {
                     doc: doc,
                     docType: getDocumentType(doc)
                 });
-                document.title = T("african law library") + doc.akomaNtoso.act.meta.publication.showAs;
             })
             .catch(function(error) {
                 console.log("error in getDocument()", error);
