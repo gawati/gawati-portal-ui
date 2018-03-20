e713c8c74cb68d75515f49b981a987f171cdd1ad
Suraj Pratap Singh <spratapsingh11@gmail.com>
2018-03-20 20:55:53 +0000

    role added

--- src/utils/GawatiAuthHelper.js
+++ src/utils/GawatiAuthHelper.js
@@ -3,55 +3,77 @@ import keycloakJson from '../configs/keycloak.json';
 
 export default class GawatiAuthHelper{
 
+ 	static init = function(){
+ 		if(window.KC == undefined){
+ 			window.KC = Keycloak(keycloakJson);
+ 		}
+ 	}
 
     static isUserLoggedIn = function(){
-	    return localStorage.getItem('authenticated')==='true';
+	    return localStorage.getItem('KC_authenticated')==='true';
 	}
 
 	static getUserName = function(){
-		return localStorage.getItem('username');
+		return localStorage.getItem('KC_username');
 	}
 
 	static login = function(){
-		const kc = Keycloak(keycloakJson);
-	    kc.init();
-	    kc.login();
+		this.init();
+	    window.KC.login();
 	}
 
 	static register = function(){
-		const kc = Keycloak(keycloakJson);
-	    kc.init();
-	    kc.register();
+		this.init();
+	    window.KC.register();
 	}
 
 	static logout = function(){
-	    const kc = Keycloak(keycloakJson);
-	    kc.init();
-	    localStorage.setItem('authenticated', 'false');
-	    localStorage.setItem('username', 'guest');
-	    kc.logout();
+	    this.init();
+	    window.KC.init();
+	    localStorage.setItem('KC_authenticated', 'false');
+	    localStorage.setItem('KC_username', 'guest');
+	    window.KC.logout();
 	}
 
 	static save = function(callback){
-	    const kc = Keycloak(keycloakJson);
-        kc.init().success(function(authenticated) {
+	    this.init();
+	    window.KC.init().success(function(authenticated) {
             if(authenticated){
-                localStorage.setItem('authenticated', 'true');
-                kc.loadUserProfile().success(function(profile) {
-                    localStorage.setItem('username', profile.username);
+            	localStorage.setItem('KC_authenticated', 'true');
+                window.KC.loadUserProfile().success(function(profile) {
+                	localStorage.setItem('KC_username', profile.username);
                     callback(true);
                 }).error(function() {
-                    localStorage.setItem('username', 'guest');
+                	localStorage.setItem('KC_username', 'guest');
                     callback(false);
                 });
             }else{
-                localStorage.setItem('authenticated', 'false');
-                localStorage.setItem('username', 'guest');
+            	localStorage.setItem('KC_authenticated', 'false');
+                localStorage.setItem('KC_username', 'guest');
                 callback(false);
             }
         }).error(function(error) {
             alert('failed to initialize'+error);
             callback(false);
         })
-	} 
+	}
+
+	static getToken = function(callback){
+		this.init();
+		window.KC.updateToken(5).success(function(refreshed) {
+	        callback(window.KC.token);
+	    }).error(function() {
+	        callback(false);
+	    });
+	}
+
+	static hasRealmRole = function(role){
+		this.init();
+		return window.KC.hasRealmRole(role);
+	}
+
+	static hasResourceRole = function(role, resource){
+		this.init();
+		return window.KC.hasRealmRole(role, resource);
+	}
 }
