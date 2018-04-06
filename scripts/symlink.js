55cde765206699242dee900665b1bf20723717a2
kohsah <ashok@hariharan.org.in>
2018-04-06 15:11:17 +0530

    Adding symlinking of themes folder in build

---
+++ scripts/symlink.js
@@ -0,0 +1,8 @@
+/**
+ * Symlinks cross plaform; We use path.resolve because windows symlinking expects absolute paths
+ */
+const fs = require('fs');
+const path = require('path');
+const from = path.resolve('./build/static/css/themes');
+const to = path.resolve('./build/themes');
+fs.symlink(from, to, "dir", function(err) { if (err !== null) {console.log("ERROR : ", err);}});
