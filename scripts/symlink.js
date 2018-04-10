a5fe1f560291d77f4bc1d101cd5d1aa41c746a5d
ccsmart <michael@gawati.org>
2018-04-10 10:20:00 +0200

    Switch to relative path symlink. For windows allow your user symlink permission prior to running npm run build. YOu can remove the permission after completion. The permission is configured in Group Pol icies of Local Computer at Computer Configuration\Windows Settings\Security Settings\Local Policies\User Rights Assignment . Please see https://docs.microsoft.com/en-us/windows/security/threat-protection/security-policy-settings/create-symbolic-links for reference.

--- scripts/symlink.js
+++ scripts/symlink.js
@@ -2,13 +2,8 @@
  * Symlinks cross plaform; We use path.resolve because windows symlinking expects absolute paths
  */
 const fs = require('fs');
-const path = require('path');
-const isWin = process.platform === 'win32';
-
-const fromPath = path.join('.','build', 'static', 'css', 'themes');
-const toPath = path.join('.', 'build', 'themes');
-
-const from = isWin ? path.resolve(fromPath) : fromPath ;
-const to = isWin ? path.resolve(toPath) : toPath ;
-
+const process = require('process');
+const from = 'static/css/themes';
+const to = 'themes';
+process.chdir('build');
 fs.symlink(from, to, "dir", function(err) { if (err !== null) {console.log("ERROR : ", err);}});
