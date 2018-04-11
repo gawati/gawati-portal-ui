486cee18344f28034570e56a94ec81d250ad5db0
ashwinibm <ashwini@baysidetech.in>
2018-04-11 17:16:59 +0530

    Get keycloak config via axios

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './i18n';
 import {setup, initLoginRequired} from './utils/GawatiAuthClient';
-import authJSON from './configs/keycloak.json';
 
 //import './index.css';
 import 'bootstrap/dist/css/bootstrap-reboot.css';
@@ -16,21 +15,24 @@ import './css/bootstrap-overrides.css';
 import './css/app-custom.css';
 import './css/app-media.css';
 
-setup(authJSON);
-
-initLoginRequired(
-    () => {
-        ReactDOM.render(
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>,
-            document.getElementById('root')
-        );
-    },
-    (error) => {
-        console.log("ERROR initLoginRequired ", "Error while logging in" );
-        alert("There was an error while initializing login");
-    }
-);
+setup().then(() => {
+    initLoginRequired(
+        () => {
+            ReactDOM.render(
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>,
+                document.getElementById('root')
+            );
+        },
+        (error) => {
+            console.log("ERROR initLoginRequired ", "Error while logging in" );
+            alert("There was an error while initializing login");
+        }
+    );
+}).catch((e) => {
+    console.log(e);
+    alert("There was an error while setting up authentication");
+});
 
 registerServiceWorker();
