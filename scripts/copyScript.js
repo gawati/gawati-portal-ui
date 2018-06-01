65a3bbf41ee7e18b496298de6b0bb3a4b2fa44e0
kohsah <ashok@hariharan.org.in>
2018-06-02 02:24:07 +0530

    FIxing build mode

--- scripts/copyScript.js
+++ scripts/copyScript.js
@@ -1,6 +1,15 @@
 var shell = require('shelljs'); 
 require('dotenv').load();
-shell.cp('-R', `src/css/themes/${process.env.REACT_APP_THEME}/locales/` , `public/`);
-//console.log(`src/css/themes/${process.env.REACT_APP_THEME}/locales/ File written at path public/locales/`);
+console.log(" PROCESS ENV = ", process.env.NODE_ENV);
 
+shell.cp(
+    '-R', 
+    `src/css/themes/${process.env.REACT_APP_THEME}/locales/`, 
+    `public/`
+);
+// remove the duplicate copy of the locales folder
+shell.rm(
+    '-rf',
+    `build/static/css/themes/${process.env.REACT_APP_THEME}/locales`
+);
 
--- scripts/symlink.js
+++ scripts/symlink.js
@@ -3,7 +3,9 @@
  */
 const fs = require('fs');
 const process = require('process');
+// symlink static/css/themes into build folder
 const from = 'static/css/themes';
 const to = 'themes';
 process.chdir('build');
 fs.symlink(from, to, "dir", function(err) { if (err !== null) {console.log("ERROR : ", err);}});
+// symlink static/css/themes/.
