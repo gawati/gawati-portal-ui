7461b0efc03aac486731ab96b0fa732e66f9d364
basant1861 <basantsharma1990@gmail.com>
2018-06-04 07:06:03 +0530

    Added error boundaries for portal fe not running and docs not loaded

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from 'reactstrap';
+import { T } from '../utils/i18nhelper';
+import oops from '../images/oops.jpg';
 
 const style = {
   width: '50%',
@@ -22,20 +24,54 @@ class ErrorBoundary extends React.Component {
       errorInfo: errorInfo
     })
   }
+
+  portalFENotRunning() {
+    return (
+      <div>
+        <Card>
+          <CardImg style={style} src={oops} />
+          <CardBody>
+            <CardTitle>{T("Portal-fe server is not running.Please start the portal-fe server")}</CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
+
+  docsNotPresent() {
+    return (
+      <div>
+        <Card>
+          <CardImg style={style} src={oops} />
+          <CardBody>
+            <CardTitle>{T("There are no documents in the database!Please load some documents.")}</CardTitle>
+          </CardBody>
+        </Card>
+      </div>
+    );
+  }
   
   render() {
-    if (this.state.errorInfo) {
-      return (
-        <div>
-          <Card>
-            <CardImg style={style} src="https://cdn.dribbble.com/users/1078347/screenshots/2799566/oops.png" />
-            <CardBody>
-              <CardTitle>Sorry something went wrong</CardTitle>
-              <CardSubtitle>Please bear with us!</CardSubtitle>
-            </CardBody>
-          </Card>
-        </div>
-      );
+    if(this.state.error){
+      if (this.state.error.toString() === "TypeError: Cannot read property 'token' of undefined") {
+        return this.portalFENotRunning();
+      }else if(this.state.error.toString() === "TypeError: Cannot read property 'map' of undefined"){
+        return this.docsNotPresent();
+      }
+      else{
+        console.log("error is "+ this.state.error.toString());
+          return (
+            <div>
+              <Card>
+                <CardImg style={style} src={oops} />
+                <CardBody>
+                  <CardTitle>{T("Sorry something went wrong")}</CardTitle>
+                  <CardSubtitle>{T("Please bear with us!")}</CardSubtitle>
+                </CardBody>
+              </Card>
+            </div>
+          );
+      }
     }
     return this.props.children;
   }  
