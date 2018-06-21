af47103b49561d87662f3400d3238ff637e66bd9
Arunadevi <l.arunadevi@gmail.com>
2018-06-21 15:26:29 +0530

    Keep language dropdown for lesser screen widths only

--- src/containers/LanguageSwitcher.js
+++ src/containers/LanguageSwitcher.js
@@ -5,6 +5,7 @@ import Select from 'react-select';
 import {NavLink} from 'react-router-dom';
 import { getLangs } from "../utils/i18nhelper";
 import { editInRoute } from "../utils/routeshelper";
+import {Aux} from "../utils/generalhelper";
 import '../css/LanguageSwitcher.css';
 import 'react-select/dist/react-select.css';
 
@@ -44,42 +45,59 @@ class LanguageSwitcher extends React.Component {
     render () {
         const _defaultSelected = filter(this.langs, { 'lang': this.props.i18n.language });
         return (
-            <div className="dropdown">
-                <div onClick={this.toggleDropDown} className="lang dropbtn">
-                    <span className={`current-lang`}>{ _defaultSelected[0].content }</span>
-                    <i className="fa fa-language fa-2x" aria-hidden="true"></i>
-                    &nbsp;
-                    <i className="fa fa-caret-down" aria-hidden="true"></i>
-                </div>
-                <ul className="lang-list dropdown-content" id="lang-dropdown"> 
-                {
-                    this.groupedArr.map(
-                        group => 
-                            (group.length > 1)  ?
-                            // <Select options={group} 
-                            //     onChange={this.onLangChange} 
-                            //     value={indexOf(group, _defaultSelected[0]) > -1 ? _defaultSelected[0] : group[0]} 
-                            //     placeholder="Select an option" 
-                            //     key={`ui-lang-${group[0].origin}`}
-                            //     valueKey='lang'
-                            //     labelKey='content'
-                            //     className={indexOf(group, _defaultSelected[0]) > -1 ? 'lang-list-item ui-lang-item ui-lang-highlight' : 'lang-list-item ui-lang-item'}
-                            //     />                      
-                            group.map(g => (
-                                <div key={ `ui-lang-${g.lang}`} 
-                                    className={ `lang-list-item ui-lang-item ${ g.lang === this.props.i18n.language ? "ui-lang-highlight": "" }`}>
-                                    <NavLink to={ editInRoute({lang:g.lang}, this.props.match) }>{g.content}</NavLink>
+            <Aux>
+                <div className="dropdown lang-list">
+                    <div onClick={this.toggleDropDown} className="lang dropbtn">
+                        <span className={`current-lang`}>{ _defaultSelected[0].content }</span>
+                        <i className="fa fa-language fa-2x" aria-hidden="true"></i>
+                        &nbsp;
+                        <i className="fa fa-caret-down" aria-hidden="true"></i>
+                    </div>
+                    <ul className="lang-list dropdown-content" id="lang-dropdown"> 
+                    {
+                        this.groupedArr.map(
+                            group => 
+                                (group.length > 1)  ?                   
+                                group.map(g => (
+                                    <div key={ `ui-lang-${g.lang}`} 
+                                        className={ `lang-list-item ui-lang-item ${ g.lang === this.props.i18n.language ? "ui-lang-highlight": "" }`}>
+                                        <NavLink to={ editInRoute({lang:g.lang}, this.props.match) }>{g.content}</NavLink>
+                                    </div>
+                                ))
+                                :
+                                <div key={ `ui-lang-${group[0].lang}`} 
+                                    className={ `lang-list-item ui-lang-item ${ group[0].lang === this.props.i18n.language ? "ui-lang-highlight": "" }`}>
+                                    <NavLink to={ editInRoute({lang:group[0].lang}, this.props.match) }>{group[0].content}</NavLink>
                                 </div>
-                            ))
-                            :
-                            <div key={ `ui-lang-${group[0].lang}`} 
-                                className={ `lang-list-item ui-lang-item ${ group[0].lang === this.props.i18n.language ? "ui-lang-highlight": "" }`}>
-                                <NavLink to={ editInRoute({lang:group[0].lang}, this.props.match) }>{group[0].content}</NavLink>
-                            </div>
-                    ) 
-                }
-                </ul>
-            </div>
+                        ) 
+                    }
+                    </ul>
+                </div>
+                <div>
+                    <ul className="list-inline"> 
+                    {
+                        this.groupedArr.map(
+                            group => 
+                                (group.length > 1)  ?
+                                <Select options={group} 
+                                    onChange={this.onLangChange} 
+                                    value={indexOf(group, _defaultSelected[0]) > -1 ? _defaultSelected[0] : group[0]} 
+                                    placeholder="Select an option" 
+                                    key={`ui-lang-${group[0].origin}`}
+                                    valueKey='lang'
+                                    labelKey='content'
+                                    className={indexOf(group, _defaultSelected[0]) > -1 ? 'list-inline-item ui-lang-item ui-lang-highlight' : 'list-inline-item ui-lang-item'}
+                                    />                      
+                                :
+                                <div key={ `ui-lang-${group[0].lang}`} 
+                                    className={ `list-inline-item ui-lang-item ${ group[0].lang === this.props.i18n.language ? "ui-lang-highlight": "" }`}>
+                                    <NavLink to={ editInRoute({lang:group[0].lang}, this.props.match) }>{group[0].content}</NavLink>
+                                </div>
+                            ) 
+                        }
+                    </ul>
+                </div>
+            </Aux>
         ); 
     }
     
